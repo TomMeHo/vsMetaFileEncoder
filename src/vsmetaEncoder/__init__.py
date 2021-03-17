381d3d09b1f159c76ffcc07de0dd586584bc53b0
Thomas Meder <10582949+TomMeHo@users.noreply.github.com>
2021-03-17 07:59:00 +0100

    corrections for delivery to pypi

--- src/vsmetaEncoder/__init__.py
+++ src/vsmetaEncoder/__init__.py
@@ -1,6 +1,5 @@
 from .vsmetaBase import VsMetaBase
-from .vsmetaInfo import VsMetaInfo
-from .vsmetaInfo import VsMetaImageInfo
+from .vsmetaInfo import VsMetaInfo, VsMetaImageInfo
 from .vsmetaListInfo import VsMetaListInfo
 from .vsmetaMovieEncoder import VsMetaMovieEncoder
 from .vsmetaSeriesEncoder import VsMetaSeriesEncoder
